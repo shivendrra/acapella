e6f5ea34345b34ad6cce43610c04b6af522877b7
Shivendra <shivharsh44@gmail.com>
2025-11-16 00:55:35 +0530

    fixed the song view

--- web/components/common/PageLoader.tsx
+++ web/components/common/PageLoader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PageLoader: React.FC = () => (
   <div className="flex items-center justify-center h-full w-full absolute inset-0">
-    <img src="https://raw.githubusercontent.com/shivendrra/acapella/0231e28f1500eb57ed6880c0fee61678cc131a7c/web/assets/svg/Loading.svg" alt="Loading..." className="w-24 h-24 animate-spin" />
+    <img src="../../assets/svg/loading.svg" alt="Loading..." className="w-24 h-24 animate-spin" />
   </div>
 );
 
