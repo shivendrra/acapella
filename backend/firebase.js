0b61a96938d8683d1aad1c860646aca5b6e5a6ab
Shivendra <shivharsh44@gmail.com>
2025-11-24 13:22:48 +0530

    rebuilding backend

--- backend/firebase.js
+++ backend/firebase.js
@@ -8,14 +8,9 @@ const MESSAGE_SENDER_ID = process.env.FIREBASE_SENDER_ID;
 const APP_ID = process.env.FIREBASE_APP_ID;
 const MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
@@ -26,6 +21,5 @@ const firebaseConfig = {
   measurementId: MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
---
+++ backend/firebase.py
@@ -0,0 +1,3 @@
+import os, json, glob
+import firebase_admin
+from firebase_admin import credentials, firestore
